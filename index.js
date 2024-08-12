ba05e5a4efdb3ee9b7c978e6dba2e12b800f89a2
quangvnhl <code398@gmail.com>
2024-08-12 22:02:52 +0700

    update, delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
 
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const {MongoClient, ObjectId, ServerApiVersion} = require("mongodb");
 const uri = "mongodb+srv://cukinacha:SMfIkhwpVg54GBnj@cluster0.b9x0pn0.mongodb.net/?retryWrites=true&w=majority";
 // const uri = "mongodb://127.0.0.1:27017";
 
@@ -28,7 +28,9 @@ app.get('/', (req, res) => {
         // const database = client.db(dbName);
         // const short = database.collection(dbColShort);
 
-        // const getShorts = short.find({}).sort({time: -1}).limit(100);
+        // const getShort = await short.findOne({encode: "hello"});
+
+        // console.log(getShort.decode)
 
         // var shortData = [];
         // for await (const short of getShorts) {
@@ -42,7 +44,7 @@ app.get('/', (req, res) => {
         //     })
         // }
        
-        console.log(createRandomCode())
+        // console.log(createRandomCode())
 
         // random characters
 
@@ -50,6 +52,7 @@ app.get('/', (req, res) => {
 
         //console.log(randomCharacters);
         res.render('index',{
+            // data: getShort.decode
             //shortData : shortData,
             //randomCharacters: randomCharacters.toUpperCase()
         });
@@ -65,8 +68,11 @@ app.get('/', (req, res) => {
     
 })
 
-app.get("/ShortLink", (req, res) => {
-    res.render('shortLink');
+app.get("/short-link", (req, res) => {
+    res.render('shortLink',{
+        actionURL: "/ShortLink",
+        actionName: 'Create',
+    });
 })
 
 app.post('/ShortLink', (req, res) => {
@@ -157,7 +163,8 @@ app.get('/list', (req, res) => {
                 encode : short.encode,
                 decode : short.decode,
                 type: short.type,
-                password: short.password
+                password: short.password,
+                id: short._id
             })
         }
        
@@ -183,6 +190,128 @@ app.get('/list', (req, res) => {
     
 })
 
+app.get('/delete/:shortId', (req, res) => {
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+        // Connect to the "insertDB" database and access its "haiku" collection
+        const database = client.db(dbName);
+        const short = database.collection(dbColShort);
+
+        const id = req.params.shortId;
+        const deleteShort = await short.deleteOne({_id: new ObjectId(id)});
+        var deleteMsg = 0;
+        if (deleteShort.deletedCount === 1) {
+            deleteMsg ="Successfully deleted one document.";
+        } else {
+            deleteMsg = "No documents matched the query. Deleted 0 documents.";
+        }
+
+        res.render('error',{
+            message: deleteMsg,
+            returnAction: "/list"
+        });
+        
+
+    } finally {
+        // Close the MongoDB client connection
+        await client.close();
+    }
+    }
+    // Run the function and handle any errors
+    run().catch(console.dir);
+})
+
+
+
+app.get('/edit/:shortCode', (req, res) => {
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+        // Connect to the "insertDB" database and access its "haiku" collection
+        const database = client.db(dbName);
+        const short = database.collection(dbColShort);
+
+        const alias = req.params.shortCode;
+        const getShort = await short.findOne({encode: alias});
+
+        res.render('shortLink',{
+            actionURL: '/ShortLink/Edit',
+            actionName: 'Edit',
+            formData : getShort,
+        });
+        
+
+    } finally {
+        // Close the MongoDB client connection
+        await client.close();
+    }
+    }
+    // Run the function and handle any errors
+    run().catch(console.dir);
+})
+
+app.post('/ShortLink/Edit', (req, res) => {
+    if(req.body.url){
+        const client = new MongoClient(uri);
+        async function run() {
+        try {
+            const database = client.db(dbName);
+            const short = database.collection(dbColShort);
+            var alias = "";
+            if(req.body.alias.length){
+                alias = req.body.alias;
+                const options = { upsert: true };
+                const updateFilter = {
+                    _id: new ObjectId(req.body.id)
+                }
+                const updateQuery = {
+                    decode: req.body.url,
+                    password: req.body.password,
+                    title: req.body.title, 
+                }
+                const queryUpdate = await short.updateOne(updateFilter, {$set: updateQuery}, options);
+                // console.log('Updated a Short URL =>', queryUpdate);
+                res.send({
+                    status: true,
+                    messageCode: `${queryUpdate.matchedCount} document(s) matched the filter, updated ${queryUpdate.modifiedCount} document(s)`,
+                    shortCode: alias
+                });
+            }
+            else{
+                alias = createRandomCode();
+                const insertResult = await short.insertOne({ 
+                    type: "url",
+                    encode: alias,
+                    decode: req.body.url, 
+                    title: req.body.title, 
+                    password: req.body.password, 
+                    time: new Date().getTime() 
+                });
+                console.log('Inserted a Short URL =>', insertResult);
+                res.send({
+                    status: true,
+                    messageCode: 1,
+                    shortCode: alias
+                });
+            }            
+            
+
+        } finally {
+            // Close the MongoDB client connection
+            await client.close();
+        }
+        }
+        // Run the function and handle any errors
+        run().catch(console.dir);
+    }
+    else{
+        res.send({
+            status: false,
+            messageCode: -1
+        });
+    }
+})
 
 app.get("/:shortCode", (req, res) => {
     const client = new MongoClient(uri);
